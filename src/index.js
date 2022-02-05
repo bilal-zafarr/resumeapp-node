13968c02d20cd6329c76c2753068b727b9dec876
Bilal Zafar <bilalzaafar@gmail.com>
2022-02-05 13:15:46 +0500

    update

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, '../public')));
 
+// app.get('/generateResume',(req, res) => {
+//     res.render(path.join(__dirname, '../public/generateResume.html'));
+// })
 
 app.listen(port, () => {
   console.log('Server running on port 3000');
